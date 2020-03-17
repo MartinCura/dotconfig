2cc547733de2ce81b69c861c76b0530e2decc581
Martin Cura <martincura@gmail.com>
2020-03-17 03:37:56 -0300

    Periodically check wifi device is available and if not restart it

---
+++ .config/i3/wifi_resetter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+while :
+do
+	sleep 5
+	STATUS=$(nmcli device show wlp59s0 | grep STATE | tr -s ' ' | cut -d':' -f2 | cut -d' ' -f2-)
+	[[ "$STATUS" != '20 (unavailable)' ]] && continue
+	echo 'wifi resetter: status '$STATUS' ! Restarting'
+	sudo /bin/systemctl restart network-manager
+	sleep 10
+done
